e59c4baa87fbb525eb4d018b45a65cf9ab121f3f
MelkiBenjamin <144021936+MelkiBenjamin@users.noreply.github.com>
2026-03-12 18:17:41 +0200

    Update Install-v2.go

--- cmd/install/Install-v2.go
+++ cmd/install/Install-v2.go
@@ -9,8 +9,7 @@ import (
 )
 
 func localBin() {
-	dir := os.Getenv("HOME") + "/.local/bin"
-	os.MkdirAll(dir, 0755)
+	os.MkdirAll(/home/runner/.local/bin, 0755)
 }
 
 func run(cmd string, args ...string) {
