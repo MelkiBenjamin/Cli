7d83b37f0e2c911591237154f8b242a23ecda1b1
MelkiBenjamin <144021936+MelkiBenjamin@users.noreply.github.com>
2026-03-24 19:45:37 +0200

    Update Test2.go

--- cmd/install/Test2.go
+++ cmd/install/Test2.go
@@ -107,11 +107,13 @@ func extractTarGz(src, dest string) {
 		  continue
 	    }
 		log.Printf("suite 4 for")
+
+	    bin := localBin()
+     	log.Printf("suite avant création destpath")
+        destpath := bin + "/" + header.Name // Concaténation avec le nom du fichier extrait
+    
+		log.Printf("suite avant outfile create")
 	}
-	bin := localBin()
-	log.Printf("suite avant création destpath")
-    destpath := bin + "/" + header.Name // Concaténation avec le nom du fichier extrait
-    log.Printf("suite avant outfile create")
 	outFile, err := os.Create(destpath) // Utilise le même nom de fichier que dans l'archive
 	if err != nil {
 		log.Fatalf("Erreur de create tar soit %s : %v", src, err)
