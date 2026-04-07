3c893ff3f0f4a5f0e74af744976356de4072ab36
MelkiBenjamin <144021936+MelkiBenjamin@users.noreply.github.com>
2026-04-07 16:37:52 +0300

    perf: remove tag resolution and use buffered extraction

---
+++ install_mise.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"archive/tar"
+	"bufio"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const latestURL = "https://github.com/jdx/mise/releases/latest/download/mise-linux-x64.tar.gz"
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func localBin() string {
+	home, err := os.UserHomeDir()
+	must(err)
+	dir := home + "/.local/bin"
+	must(os.MkdirAll(dir, 0o755))
+	return dir
+}
+
+func extractMiseFromURL(url, dir string) {
+	resp, err := http.Get(url)
+	must(err)
+	defer resp.Body.Close()
+
+	buffered := bufio.NewReaderSize(resp.Body, 256*1024)
+	gz, err := gzip.NewReader(buffered)
+	must(err)
+	defer gz.Close()
+
+	tr := tar.NewReader(gz)
+	for {
+		h, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		must(err)
+		parts := strings.Split(h.Name, "/")
+		if h.Typeflag != tar.TypeReg || parts[len(parts)-1] != "mise" {
+			continue
+		}
+		bin, err := os.OpenFile(dir+"/mise", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o755)
+		must(err)
+		_, err = io.Copy(bin, tr)
+		must(err)
+		_ = bin.Close()
+		return
+	}
+	panic("binaire mise introuvable")
+}
+
+func main() {
+	dir := localBin()
+	extractMiseFromURL(latestURL, dir)
+	fmt.Println("mise installé dans", dir+"/mise")
+}
