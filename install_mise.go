842abae472e28760920b0cb41d9d22cb9ea77795
MelkiBenjamin <144021936+MelkiBenjamin@users.noreply.github.com>
2026-04-10 00:08:32 +0300

    Update install_mise.go

--- install_mise.go
+++ install_mise.go
@@ -44,10 +44,10 @@ func extractMiseFromURL(url, dir string) {
 			break
 		}
 		must(err)
-//		parts := strings.Split(h.Name, "/")
-//		if h.Typeflag != tar.TypeReg || parts[len(parts)-1] != "mise" {
-//			continue
-//		}
+		parts := strings.Split(h.Name, "/")
+		if h.Typeflag != tar.TypeReg || parts[len(parts)-1] != "mise" {
+			continue
+		}
 		bin, err := os.OpenFile(dir+"/mise", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o755)
 		must(err)
 		_, err = io.Copy(bin, tr)
