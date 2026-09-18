38c49fa7e02c09c4d42eb7bb83489ee6beae7a59
MelkiBenjamin <144021936+MelkiBenjamin@users.noreply.github.com>
2026-09-18 14:30:44 +0200

    Update install_mise.go

--- install_mise.go
+++ install_mise.go
@@ -401,7 +401,7 @@ server:
       url: "http://localhost:3000"
       uuid: "%s"
       token: "%s"
-`, forgejoDir, runnerUUID, sharedSecret)
+`, runnerUUID, sharedSecret)
 
 	must(os.WriteFile(configPath, []byte(runnerConfig), 0o600))
 	fmt.Println("[+] Configuration .forgejo-runner générée : %s\n", configPath)
