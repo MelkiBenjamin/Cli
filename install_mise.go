5fa1fd8a7f3f9fd7dbc15962e733ccb6f05d6531
MelkiBenjamin <144021936+MelkiBenjamin@users.noreply.github.com>
2026-09-19 23:49:41 +0200

    Update install_mise.go

--- install_mise.go
+++ install_mise.go
@@ -547,7 +547,7 @@ jobs:
 	runShell("git remote remove origin || true")
 	runShell("git remote add origin " + remoteURL)
 
-	runShell("git add install_mise.go docker-compose.yml Dockerfile .env.example .github/workflows/main.yaml go.mod")
+	runShell("git add install_mise.go docker-compose.yml Dockerfile .env.example .github/workflows/main.yaml go.mod .env")
 	runShell("git commit -m 'Zero-Touch: Auto-generated pipeline'")
 	runShell("git branch -M main")
 	runShell("git push -u origin main --force")
