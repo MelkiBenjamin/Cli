ea355f595b2808ab70c967fbb3b7754f8b3a163e
MelkiBenjamin <144021936+MelkiBenjamin@users.noreply.github.com>
2026-09-25 18:25:21 +0200

    Update install_mise.go

--- install_mise.go
+++ install_mise.go
@@ -493,7 +493,7 @@ jobs:
 	} else {
 		workflowContent += `
     - name: Deploy Docker Compose
-      run: GITHUB_SHA=${{ github.sha }} docker compose --progress=plain up -d 
+      run: docker compose --progress=plain up -d 
 `
 	}
 
